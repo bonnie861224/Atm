b75cf4a8c0ce8c44fbbb2ff332d8b93a37fe27ca
bonnie chang <b22379222@yahoo.com.tw>
2017-12-19 11:20:20 +0800

    spinner

--- app/src/main/java/com/bonnie/atm/MainActivity.java
+++ app/src/main/java/com/bonnie/atm/MainActivity.java
@@ -16,63 +16,64 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_LOGIN = 102;
     private final static int REQUEST_USERINFO = 105;
     boolean logon = false;
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode){
-            case REQUEST_LOGIN:
-            if (resultCode ==RESULT_OK) {
-                String userid = data.getStringExtra("LOGIN_USERID");
-                String passwd = data.getStringExtra("LOGIN_PASSWD");
-                Toast.makeText(this, "LOGIN_USERID : "+userid, Toast.LENGTH_LONG).show();
-                getSharedPreferences("atm",MODE_PRIVATE)
-                        .edit()
-                        .putString("USERID",userid)
-                        .apply();
-                Log.d("RESULT", userid + "/" + passwd);
-            }else{
-                finish();
-            }
-        break;
-            case  REQUEST_USERINFO:
-        if (resultCode ==RESULT_OK) {
-            String name = data.getStringExtra("LOGIN_NAME");
-            String phone = data.getStringExtra("LOGIN_PHONE");
-            Toast.makeText(this, "LOGIN_NAME : "+name, Toast.LENGTH_LONG).show();
-            Toast.makeText(this, "LOGIN_PHONE : "+phone, Toast.LENGTH_LONG).show();
-            Log.d("RESULT", name + "/" + phone);
-        }else{
-            finish();
-        }
-        break;
-    }
-}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // startActivity(new Intent(this, Ch8Activity.class));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         if (!logon) {
             Intent intent = new Intent(this, LoginActivity.class);
             //  startActivity(intent);
-            startActivityForResult(intent,REQUEST_LOGIN);
-
+            startActivityForResult(intent, REQUEST_LOGIN);
         }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent().setClass(MainActivity.this,UserInfoActivity.class);
-                startActivityForResult(intent,REQUEST_USERINFO);
+                Intent intent = new Intent().setClass(MainActivity.this, UserInfoActivity.class);
+                startActivityForResult(intent, REQUEST_USERINFO);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action",null).show();
+                        .setAction("Action", null).show();
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_LOGIN:
+                if (resultCode == RESULT_OK) {
+                    String userid = data.getStringExtra("LOGIN_USERID");
+                    String passwd = data.getStringExtra("LOGIN_PASSWD");
+                    Toast.makeText(this, "LOGIN_USERID : " + userid, Toast.LENGTH_LONG).show();
+                    getSharedPreferences("atm", MODE_PRIVATE)
+                            .edit()
+                            .putString("USERID", userid)
+                            .apply();
+                    Log.d("RESULT", userid + "/" + passwd);
+                } else {
+                    finish();
+                }
+                break;
+            case REQUEST_USERINFO:
+                if (resultCode == RESULT_OK) {
+                    String name = data.getStringExtra("LOGIN_NAME");
+                    String phone = data.getStringExtra("LOGIN_PHONE");
+                    Toast.makeText(this, "LOGIN_NAME : " + name, Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "LOGIN_PHONE : " + phone, Toast.LENGTH_LONG).show();
+                    Log.d("RESULT", name + "/" + phone);
+                } else {
+                    finish();
+                }
+                break;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/bonnie/atm/UserInfoActivity.java
+++ app/src/main/java/com/bonnie/atm/UserInfoActivity.java
@@ -3,24 +3,38 @@ package com.bonnie.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class UserInfoActivity extends AppCompatActivity {
+    private static final String TAG = Ch8Activity.class.getSimpleName(); ;
     private EditText edName;
     private EditText edPhone;
+    private  Spinner ages;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
         edName = (EditText) findViewById(R.id.name);
         edPhone = (EditText) findViewById(R.id.phone);
-        String name = getSharedPreferences("atm",MODE_PRIVATE)
-                .getString("NAME","");
+        String name = getSharedPreferences("atm",MODE_PRIVATE).getString("NAME","");
         edName.setText(name);
+        ages = (Spinner) findViewById(R.id.ages);
+        ArrayList<String> data = new ArrayList<>();
+        for (int i=15 ;i<=40;i++){
+            data.add(i+"");
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1 ,data);
+        ages.setAdapter(adapter);
     }
     public void info(View view) {
+        Log.d (TAG,"OK:"+ages.getSelectedItem().toString());
         String name = edName.getText().toString();
         String phone = edPhone.getText().toString();
         getIntent().putExtra("LOGIN_NAME", name);
