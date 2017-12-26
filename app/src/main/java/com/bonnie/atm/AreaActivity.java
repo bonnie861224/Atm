194aee95a9206f5c0cb608bcb0e3df867eb51585
bonny chang <b22379222@gmail.com>
2017-12-26 16:35:02 +0800

    spinner & list

--- app/src/main/java/com/bonnie/atm/AreaActivity.java
+++ app/src/main/java/com/bonnie/atm/AreaActivity.java
@@ -6,35 +6,43 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class AreaActivity extends CityActivity {
-//public class AreaActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class AreaActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private String[] area;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
-        final ListView area = (ListView) findViewById(R.id.area);
-        area.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            String[] data=null;
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        data = new String[]{"中正區", "暖暖區", "八堵區"};
-                        break;
-                    case 1:
-                        data = new String[]{"永和區", "板橋區", "新莊區"};
-                        break;
-                    case 2:
-                        data = new String[]{"信義區", "大安區", "士林區"};
-                        break;
-                }
-                ArrayAdapter adapter = new ArrayAdapter(
-                        AreaActivity.this, android.R.layout.simple_list_item_1,
-                        data);
-                area.setAdapter(adapter);
-            }
-            });
+        ListView areas = (ListView) findViewById(R.id.area);
+        int city = getIntent().getIntExtra("EXTRA_CITY", 0);
+        area =null;
+        switch (city) {
+            case 0:
+                String[] data0 = new String[]{"中正區", "暖暖區", "八堵區"};
+                this.area = data0;
+                break;
+            case 1:
+                String[] data1 = new String[]{"永和區", "板橋區", "新莊區"};
+                this.area = data1;
+                break;
+            case 2:
+                String[] data2 = new String[]{"信義區", "大安區", "士林區"};
+                this.area = data2;
+                break;
         }
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, area);
+        areas.setAdapter(adapter);
+        areas.setOnItemClickListener(this);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        getIntent().putExtra("EXTRA_AREA", area[i]);
+        setResult(RESULT_OK, getIntent());
+        finish();
+    }
+}
--- app/src/main/java/com/bonnie/atm/CityActivity.java
+++ app/src/main/java/com/bonnie/atm/CityActivity.java
@@ -10,38 +10,47 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
+import static com.bonnie.atm.R.id.parent;
+
 public class CityActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private static final String TAG = CityActivity.class.getSimpleName();
-    private String[] cities;
+    private ListView city;
     private final static int REQUEST_CITY = 105;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
-        ListView city = (ListView) findViewById(R.id.city);
-        cities = new String[]{"基隆市","新北市","台北市"};
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,cities);
+        city = (ListView) findViewById(R.id.city);
+        String[] cities = {"基隆市", "新北市", "台北市"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, cities);
         city.setAdapter(adapter);
         city.setOnItemClickListener(this);
-        city.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long i) {
-        Intent intent = new Intent(CityActivity.this, AreaActivity.class);
-        startActivityForResult(intent ,REQUEST_CITY);
-        }
-    });
-
-}
+    }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-        Log.d(TAG ,"onItemClick: " + position + "/" + cities[position]);
-        TextView tv =(TextView) view;
-        tv.setText(tv.getText()+"*");
+        Intent intent = new Intent(this, AreaActivity.class);
+        intent.putExtra("EXTRA_CITY", position);
+        startActivityForResult(intent, REQUEST_CITY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            String area = data.getStringExtra("EXTRA_AREA");
+            Toast.makeText(this, area, Toast.LENGTH_LONG).show();
+        }
     }
 }
-
-
+     /*  @Override
+         public void onItemClick (AdapterView < ? > adapterView, View view,int position, long l){
+         Log.d(TAG, "onItemClick: " + position + "/" + cities[position]);
+         TextView tv =(TextView) view;
+         tv.setText(tv.getText()+"*");
+       }*/
